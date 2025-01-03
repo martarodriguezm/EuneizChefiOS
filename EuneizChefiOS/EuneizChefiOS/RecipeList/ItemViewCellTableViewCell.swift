3fbf8489cc2a2821be7bfbb5b9ffaf00a4a85ac2
izarorubio <izaro.rubio@euneiz.com>
2025-01-03 02:58:49 +0100

    Barra de busqueda funcional

---
+++ EuneizChefiOS/EuneizChefiOS/RecipeList/ItemViewCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemViewCellTableViewCell.swift
+//  EuneizChefiOS
+//
+//  Created by Garoa Rojí Rodrigues on 3/1/25.
+//
+
+import UIKit
+
+class ItemViewCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

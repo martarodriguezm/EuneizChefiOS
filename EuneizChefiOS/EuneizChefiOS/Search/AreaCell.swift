73ffce7bd4a5dfc63a3d1ad262b516fb6feffa68
izarorubio <izaro.rubio@euneiz.com>
2025-01-08 06:59:25 +0100

    Storyboard 1.0

--- EuneizChefiOS/EuneizChefiOS/Search/AreaCell.swift
+++ EuneizChefiOS/EuneizChefiOS/Search/AreaCell.swift
@@ -16,4 +16,15 @@ class AreaCell: UICollectionViewCell {
     func configureCell(area: String) {
         areaNameLabel.text = area
     }
+    
+    override func awakeFromNib() {
+           super.awakeFromNib()
+           
+           // Redondear las esquinas de la celda
+           self.layer.cornerRadius = 10
+           self.layer.masksToBounds = true
+           // Agregar un borde a la celda
+           self.layer.borderWidth = 3
+           self.layer.borderColor =  UIColor(red: 1.0, green: 0.8, blue: 0.7, alpha: 1.0).cgColor
+       }
 }
--- EuneizChefiOS/EuneizChefiOS/Search/CategoryCell.swift
+++ EuneizChefiOS/EuneizChefiOS/Search/CategoryCell.swift
@@ -13,4 +13,14 @@ class CategoryCell: UICollectionViewCell {
     func configureCell(category: String) {
         categoryLabel.text = category
     }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Redondear las esquinas de la celda
+        self.layer.cornerRadius = 10
+        self.layer.masksToBounds = true
+        
+        // Agregar un borde a la celda
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.gray.cgColor  // Color gris para el borde
+    }
 }
